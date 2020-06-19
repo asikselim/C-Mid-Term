8f35ffbb9f16740ffb85fa180efe0d99f37cf5e0
Selim Aşık <selim.asik@hotmail.com>
2020-06-19 10:57:24 +0300

    Add files via upload

---
+++ piramitCizme/piramitCizme/piramit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace piramitCizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sayi, boşluklar;
+
+            while (true)//sonsuz döngü için
+            {
+                Console.Write("1-9 arası bir sayı giriniz : ");
+
+                sayi = Convert.ToInt32(Console.ReadLine());
+
+                boşluklar = sayi - 1;
+
+                for (int i = 1; i <= sayi; i++)
+                {
+                    for (boşluklar = 1; boşluklar <= (sayi - i); boşluklar++)
+                    {
+                        Console.Write(" ");
+                    }
+
+                    for (int j = 1; j <= i; j++)
+                    {
+                        Console.Write(j);
+                    }
+
+                    for (int k = (i - 1); k >= 1; k--)
+                    {
+                        Console.Write(k);
+                    }
+
+                    Console.WriteLine();
+                    
+
+                }
+            }
+        }
+    }
+}
---
+++ vizeFinalProjeHesaplama/vizeFinalProjeHesaplama/ortalama.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace vizeFinalProjeHesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double vize, final, proje, ortalama;
+            Console.Write("Vize notunu giriniz: ");
+            vize = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Final notunu giriniz: ");
+            final = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Proje notunu giriniz: ");
+            proje = Convert.ToDouble(Console.ReadLine());
+
+
+            ortalama = Convert.ToDouble(vize * 30 / 100) + (final * 40 / 100) + (proje * 30 / 100);
+
+            Console.WriteLine("Ortalamanız = {0}", ortalama);
+
+            if (ortalama>=60)
+            {
+                Console.WriteLine("Ortalamanız:" + ortalama.ToString()+ " "+"Geçtiniz");
+            }
+            else if (ortalama<=60 && ortalama>=40)
+            {
+                Console.WriteLine("Ortalamanız:" + ortalama.ToString() +  " "+ "Bütünleme sınavına girmeniz gerekiyor");
+            }
+            else if(ortalama<40)
+            {
+                Console.WriteLine("Ortalamanız:" + ortalama.ToString() + " " + "Dersten kaldınız");
+            }
+                
+
+
+            Console.ReadKey();
+        }
+    }
+}
